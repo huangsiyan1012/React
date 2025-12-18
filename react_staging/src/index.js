8025ff663eaecac255fffd887ef7503a81aad103
H <huangsiyan1012@163.com>
2025-12-18 13:49:50 +0800

    初始化目录结构介绍

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 入口文件
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+// 用于记录文件性能
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
