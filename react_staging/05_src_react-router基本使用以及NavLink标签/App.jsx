1a25f52eaefa040bee256cc6c6f19f4a3c282dc4
H <huangsiyan1012@163.com>
2025-12-24 13:42:40 +0800

    switch的使用

--- react_staging/05_src_react-router基本使用以及NavLink标签/App.jsx
+++ react_staging/05_src_react-router基本使用以及NavLink标签/App.jsx
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Header from "./components/Header";
 import MyNavLink from "./components/MyNavLink";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Test from "./pages/Test";
 
 export default class App extends Component {
   render() {
@@ -27,20 +26,16 @@ export default class App extends Component {
                 </a> */}
 
                 {/* 在React中靠路由链接实现切换组件 - 编写路由链接 */}
-                <MyNavLink to="/home">Home</MyNavLink>
-                <MyNavLink to="/about">About</MyNavLink>
+                <MyNavLink to="/about">Home</MyNavLink>
+                <MyNavLink to="/home">About</MyNavLink>
               </div>
             </div>
             <div className="col-xs-6">
               <div className="panel">
                 <div className="panel-body">
                   {/* 注册路由 */}
-                  {/* Switch组件保证一个路由途径对应一个组件 */}
-                  <Switch>
-                    <Route path="/about" component={About}></Route>
-                    <Route path="/home" component={Home}></Route>
-                    <Route path="/home" component={Test}></Route>
-                  </Switch>
+                  <Route path="/about" component={About}></Route>
+                  <Route path="/home" component={Home}></Route>
                 </div>
               </div>
             </div>
---
+++ react_staging/src/pages/Test/index.jsx
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class Test extends Component {
+  render() {
+    return <div>Test</div>;
+  }
+}
