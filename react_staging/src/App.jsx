c37361440c85e2f4a86d3229edddb4c65157b5a4
H <huangsiyan1012@163.com>
2025-12-30 14:21:26 +0800

    优化-Providr使用

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
-import Count from './containers/Count'
-import store from './redux/store'
+import { Component } from "react";
+import Count from "./containers/Count";
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				{/* 给容器组件传递store */}
-				<Count store={store} />
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <Count />
+      </div>
+    );
+  }
 }
